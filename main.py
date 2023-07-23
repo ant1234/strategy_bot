68ca080082b70c69c3f4e09c01e23895df37d6b0
unknown <antonyvidetta1986@gmail.com>
2023-07-23 16:51:28 +1200

    Set up linking linking to binance

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import tkinter as tk
 import logging
+from binance_futures import write_log
 
 # logging used for debugging. 
 logger = logging.getLogger()
 
 # set logging level to show info logging in the console.
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # show time, level names and messages 
 stream_handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(logging.INFO)
+
+# output logging messages to file.
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
 logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
-logger.debug('This message is useful when debugging the program.')
-logger.info('This shows some basic information in the program.')
-logger.warning('This shows something to pay close attention to.')
-logger.error('This shows that something has gone wrong in the program.') 
+if __name__ == '__main__':
+    # tkinter ui.
+    root = tk.Tk()
 
-# tkinter ui.
-root = tk.Tk()
-
-# prevent termination of program after running.
-root.mainloop()
+    # prevent termination of program after running.
+    root.mainloop()
