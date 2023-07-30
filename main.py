902edda7502ced60aa82eec9e629621dde76a257
unknown <antonyvidetta1986@gmail.com>
2023-07-30 21:45:21 +1200

    Get connection and first 2 methods working for bitmex class exchange

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import logging
 import pprint
 import os
 from connectors.binance_futures import BinanceFuturesClient
+from connectors.bitmex_futures import BitmexFuturesClient
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Access environment variables
-testnet_public_key = os.environ.get('BINANCE_TESTNET_PUBLIC_KEY')
-testnet_secret_key = os.environ.get('BINANCE_TESTNET_SECRET_KEY')
+# Access environment variables for binance.
+binance_testnet_public_key = os.environ.get('BINANCE_TESTNET_PUBLIC_KEY')
+binance_testnet_secret_key = os.environ.get('BINANCE_TESTNET_SECRET_KEY')
+
+# Access environment variables for bitmex.
+bitmex_testnet_public_key = os.environ.get('BITMEX_TESTNET_PUBLIC_KEY')
+bitmex_testnet_secret_key = os.environ.get('BITMEX_TESTNET_SECRET_KEY')
 
 # logging used for debugging. 
 logger = logging.getLogger()
@@ -34,7 +39,12 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':
 
-    binance = BinanceFuturesClient(testnet_public_key, testnet_secret_key, True)
+    binance = BinanceFuturesClient(binance_testnet_public_key, binance_testnet_secret_key, True)
+    bitmex = BitmexFuturesClient(bitmex_testnet_public_key, bitmex_testnet_secret_key, True)
+
+    # print(bitmex.contracts('XBTUSD').base_asset, bitmex.contracts('XBTUSD').price_decimals)
+    # print(bitmex.balances('XBt').wallet_balance)
+
 
     # tkinter ui.
     root = tk.Tk()
