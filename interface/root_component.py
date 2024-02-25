f9a2045d29bf9f1687bee488764617d9ae7653db
ant1234 <antonyvidetta1986@gmail.com>
2024-02-25 17:05:46 +1300

    implement read from database functionality

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter.messagebox import askquestion
 import logging
+import json
 
 from connectors.bitmex import BitmexClient
 from connectors.binance_futures import BinanceFuturesClient
@@ -155,4 +156,41 @@ class Root(tk.Tk):
         self.after(1500, self._update_ui)
     
     def _save_workspace(self):
+
         # Watchlist
+
+        watchlist_symbols = []
+
+        for key, value in self._watchlist_frame.body_widgets['symbol'].items():
+            symbol = value.cget("text")
+            exchange = self._watchlist_frame.body_widgets['exchange'][key].cget("text")
+            watchlist_symbols.append((symbol, exchange, ))
+
+        self._watchlist_frame.db.save("watchlist", watchlist_symbols)
+
+        # Strategies 
+        strategies = []
+
+        strat_widgets = self._strategy_frame.body_widgets
+
+        for b_index in strat_widgets['contract']:
+
+            strategy_type = strat_widgets['strategy_type_var'][b_index].get()
+            contract = strat_widgets['contract_var'][b_index].get()
+            timeframe = strat_widgets['timeframe_var'][b_index].get()
+            balance_pct = strat_widgets['balance_pct'][b_index].get()
+            take_profit = strat_widgets['take_profit'][b_index].get()
+            stop_loss = strat_widgets['stop_loss'][b_index].get()
+
+            extra_params = dict()
+
+            for param in self._strategy_frame.extra_params[strategy_type]:
+                code_name = param['code_name']
+
+                extra_params[code_name] = self._strategy_frame.additional_parameters[b_index][code_name]
+
+            strategies.append((strategy_type, contract, timeframe, balance_pct, take_profit, stop_loss, json.dumps(extra_params), ))
+
+        self._strategy_frame.db.save("strategies", strategies)
+
+        self.logging_frame.add_log("Workspace saved")
